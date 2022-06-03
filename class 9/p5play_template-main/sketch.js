919fb62832f153299ee33ff37b05dc7f5b754d0b
Ishaanop123 <106819562+Ishaanop123@users.noreply.github.com>
2022-06-03 18:45:53 +0530

    Add files via upload

---
+++ class 9/p5play_template-main/sketch.js
@@ -0,0 +1,40 @@
+var box ;
+function setup() {
+  createCanvas(400,400);
+box = createSprite(200,200,50,50);
+box.shapeColor = "cyan";
+
+
+}
+
+function draw() 
+{
+  background(30);
+
+if(keyIsDown(DOWN_ARROW)){
+  box.position.y +=2;
+}
+
+if(keyIsDown(UP_ARROW)){
+  box.position.y -=2;
+}
+
+
+
+if(keyIsDown(LEFT_ARROW)){
+  box.position.x -=2;
+}
+
+if(keyIsDown( RIGHT_ARROW)){
+  box.position.x +=2;
+}
+
+
+drawSprites()
+
+
+}
+
+
+
+
